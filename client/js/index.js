2287c4848c88726e2a935035f90a9a1a4306cbdc
nighca <nighca@live.cn>
2014-08-21 18:22:40 +0800

    adjust file upload behavior

--- client/js/index.js
+++ client/js/index.js
@@ -3,20 +3,24 @@ $(function(){
     // file uploader
     !function(){
 
-        var fileInput = $('#file-in'),
-            addrInput = $('#addr-in');
+        var form = $('#form'),
+            fileName = $('#file-name'),
+            fileButton = $('#file-btn'),
+            fileInput = $('#file-in'),
+            urlInput = $('#url-in');
 
-        var validExts = ['zip', 'js'];
+        var validExts = ['zip', 'js', 'md'];
+
+        // set accepted file types
+        fileInput.attr('accept', '.' + validExts.join(',.'));
 
         // switch mode based on file selected or not
         var updateInput = function(filePath){
             if(filePath){
                 var name = /([^\/\\]+)$/.exec(filePath)[1];
-                addrInput.val(name);
-                addrInput[0].disabled = true;
+                fileName.show().text('File: ' + name);
             }else{
-                addrInput.val('');
-                addrInput[0].disabled = false;
+                fileName.hide().text('');
             }
         };
 
@@ -39,6 +43,21 @@ $(function(){
         // respond to file select
         fileInput.on('change', function(e){
             updateInput(this.value);
+        });
+
+        // listen to file name click
+        fileName.on('click', function(){
+            fileInput.click();
+        });
+
+        // listen to file-drag
+        form.on('drop', function(e){
+            e.preventDefault();
+
+            var fileList = e.dataTransfer.files;
+            if(fileList && fileList.length){
+                var file = fileList[0];
+            }
         });
 
     }();
